b8e0ec50394527d44efd9c5a6848918ec794ceac
Anant Kumar Jha <jhasonu136@gmail.com>
2025-07-19 02:36:08 +0530

    Fix: Update cookie settings for cross-site auth

--- foodapp-anant/server/utils/generateToken.ts
+++ foodapp-anant/server/utils/generateToken.ts
@@ -1,9 +1,24 @@
-import jwt from "jsonwebtoken";
-import { IUserDocument } from "../models/user.model";
 import { Response } from "express";
+import jwt from "jsonwebtoken";
+import { IUserDocument } from "../models/user.model"; // Assuming you have a UserDocument type
 
-export const generateToken = (res:Response, user:IUserDocument ) => {
-    const token = jwt.sign({userId:user._id}, process.env.SECRET_KEY!, {expiresIn:'1d'});
-    res.cookie("token", token, {httpOnly:true, sameSite:'strict', maxAge:24*60*60*1000});
+export const generateToken = (res: Response, user: IUserDocument) => {
+    // Create the token
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY!, {
+        expiresIn: '1d' // Token will expire in 1 day
+    });
+
+    // Define the cookie options for production
+    const cookieOptions = {
+        httpOnly: true,
+        maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day in milliseconds
+        secure: true, // MUST be true for cross-site cookies on HTTPS
+        sameSite: "none" as const // Allow cookie to be sent on cross-site requests
+    };
+
+    // Set the cookie
+    res.cookie("token", token, cookieOptions);
+    
+    // You can still return the token if your frontend needs it for some reason
     return token;
-}
+};
